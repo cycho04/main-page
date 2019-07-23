09e3d2fd939e0f312c71ea39ab606cd2228fb169
Chris Cho <chrisycho04@gmail.com>
2019-07-22 18:03:05 -0700

    changed navbar. need to add copy to clipboard

--- dist/js/main.js
+++ dist/js/main.js
@@ -5,6 +5,7 @@ const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
 const menuBranding = document.querySelector(".menu-branding");
 const navItems = document.querySelectorAll(".nav-item");
+const emailButton = document.getElementById('email');
 
 //set Initial state of menu
 let showMenu = false;
